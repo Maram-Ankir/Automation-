496c8acb8eef24180fa888f0babb0240b4f7798f
Maram-Ankir <maramankir5@gmail.com>
2021-08-19 08:48:30 +0300

    Editing

--- automation/automation.py
+++ automation/automation.py
@@ -5,8 +5,7 @@ from faker import Faker
 fake = Faker('en_US')
 
 potential_contacts = ""
-
-# existing_contacts = ""
+existing_contacts=""
 
 
 for i in range(100):
@@ -18,11 +17,10 @@ for i in range(100):
 
     if i % 7 == 0: 
         potential_contacts += " " + email + " "
-        potential_contacts + fake.sentence()
 
     if i % 9 == 0: 
         potential_contacts += phone_number
-        potential_contacts += fake.paragraph()
+
 
     if i % 5 == 0:
         existing_contacts += email + "\n"
@@ -34,4 +32,21 @@ for i in range(100):
 with open("potential-contacts.txt", "w+") as f:
     f.write(potential_contacts)
     
+
+
+with open("potential-contacts.txt","r") as f:
+ file_content=f.read()
+
+match = re.findall(r'[\w.+-]+@[\w-]+\.[\w.-]+', file_content)
+match=set(match)
+
+with open("email.txt", "w") as f:
+    for element in match:
+        f.write(element + "\n")
+
+phone = re.findall(r'\d{3}-\d{3}-\d{4}|\d{3}-\d{4}', file_content)
+
+with open("phone.txt", "w") as f:
+    for element in phone:
+         f.write( element + "\n")
 
